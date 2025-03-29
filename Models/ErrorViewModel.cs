2597a90200ed98a0c09941f9216006b70990cff2
Bry <bryan2910arnold@gmail.com>
2025-03-28 20:57:08 -0500

    comienzo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Primera_pc_Teoria.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
